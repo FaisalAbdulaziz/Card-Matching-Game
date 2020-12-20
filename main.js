64489f953916bfacbb65b0ecea583b9d0ac3b764
faisalabdulaziz <xfaisalx123@gmail.com>
2020-12-20 15:15:25 +0300

    remove the event if cards are the same

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
+let active = false;
+let firstCard;
+let secondCard;
+let cards = $('.card');
 const flip = function(){
     this.classList.toggle('flip');
+    if(active){
+        secondCard = this;
+        active=false;
+        console.log('sec',secondCard, active)
+        if(($(firstCard).find("img")[0].src) === ($(secondCard).find("img")[0].src)){
+            console.log("kjkjkjkjjkjkjkjk")
+            $(firstCard).unbind()
+            $(secondCard).unbind()
+        }else{
+            console.log("oooooooooooo")
+        }
+    }else{
+        active=true;
+        firstCard=this;
+        console.log('f',firstCard ,active)
+    }
+    
+
 }
-
-let cards = $('.card');
-
 cards.on("click",flip)
+
+
+
+
+
+
 
